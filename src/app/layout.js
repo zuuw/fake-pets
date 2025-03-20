eca7a7a2453131abb55e89723448a561d945be36
zuuw <codymcleod02@gmail.com>
2025-03-19 19:02:48 -0600

    Moved pages and components to src.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
